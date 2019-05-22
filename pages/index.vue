c85f8004b7288ff22902be95d0f20de2d94289d6
Boadzie Daniel <boadziedaniel43@gmail.com>
2019-05-22 21:38:42 +0000

    Added skills section

--- pages/index.vue
+++ pages/index.vue
@@ -61,13 +61,29 @@
               <article class="tile is-child box about">
                 <h1 class="is-size-4 is-uppercase">About Me</h1>
                 <hr>
-                <p>lorem10</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ratione nesciunt accusantium
+                  laborum, suscipit ut molestias iusto odio molestiae.</p>
               </article>
               <!-- the skills -->
               <article class="tile is-child box">
                 <h1 class="is-size-4 is-uppercase">Skills</h1>
                 <hr>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, obcaecati veniam. Eligendi quidem ipsam quia magni a culpa, dolorem rerum.</p>
+                <div class="progress-wrapper">
+                  <progress class="progress is-danger is-large" value="81" max="100">90%</progress>
+                  <p class="progress-value has-text-white">VENoM-Stack(Vue.js)</p>
+                </div>
+                <div class="progress-wrapper">
+                  <progress class="progress is-primary is-large" value="79" max="100">90%</progress>
+                  <p class="progress-value has-text-white">Data Science</p>
+                </div>
+                <div class="progress-wrapper">
+                  <progress class="progress is-grey is-large" value="75" max="100">90%</progress>
+                  <p class="progress-value has-text-white">Artificial Intelligence</p>
+                </div>
+                <div class="progress-wrapper">
+                  <progress class="progress is-info is-large" value="78" max="100">90%</progress>
+                  <p class="progress-value has-text-white">DevOps</p>
+                </div>
               </article>
             </div>
             <div class="tile is-parent">
@@ -80,16 +96,19 @@
           </div>
           <div class="tile is-parent">
             <article class="tile is-child box">
-              <h1 class="is-size-4 is-uppercase">Projects</h1>
-              <hr>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo esse facilis provident, incidunt sed qui totam atque dicta illo! Cumque labore laudantium odio, blanditiis ipsa minima nobis eius totam minus amet, fugit dolore placeat delectus dolorum? Odio mollitia culpa totam?
+              <h1 class="is-size-4 is-uppercase">Certificates & Honors</h1>
+              <hr>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo esse facilis provident, incidunt
+              sed qui totam atque dicta illo! Cumque labore laudantium odio, blanditiis ipsa minima nobis eius totam
+              minus amet, fugit dolore placeat delectus dolorum? Odio mollitia culpa totam?
             </article>
           </div>
         </div>
         <div class="tile is-parent">
           <article class="tile is-child box">
             <!-- Put any content you want -->
-            <h1 class="is-size-4 is-uppercase">Certificates & Honors</h1>
+            <h1 class="is-size-4 is-uppercase">Projects</h1>
             <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, doloribus.</p>
           </article>
         </div>
       </div>
@@ -98,33 +117,64 @@
 </template>
 
 <script>
-export default {
-  components: {}
-};
+  export default {
+    components: {}
+  };
+
 </script>
 
 <style>
-.down {
-  margin-bottom: em !important;
-}
+  .down {
+    margin-bottom: em !important;
+  }
 
-.card {
-  padding: 1em;
-  margin-top: 1em !important;
-}
+  .card {
+    padding: 1em;
+    margin-top: 1em !important;
+  }
 
-img {
-  width: 1em;
-  height: 30px;
-}
+  img {
+    width: 1em;
+    height: 30px;
+  }
 
-.cover {
-  background: #17202a;
-  border-radius: 5px;
-}
+  .cover {
+    background: #17202a;
+    border-radius: 5px;
+  }
 
-hr {
-  border: 1px solid #ea6;
-  margin-top: 6px;
-}
+  .progress-wrapper {
+    position: relative;
+  }
+
+  .progress-value {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: calc(1rem / 1.5);
+    line-height: 1rem;
+    font-weight: bold;
+  }
+
+  .progress.is-small+.progress-value {
+    font-size: calc(0.75rem / 1.5);
+    line-height: 0.75rem;
+  }
+
+  .progress.is-medium+.progress-value {
+    font-size: calc(1.25rem / 1.5);
+    line-height: 1.25rem;
+  }
+
+  .progress.is-large+.progress-value {
+    font-size: calc(1.5rem / 1.5);
+    line-height: 1.5rem;
+  }
+
+  hr {
+    border: 1px solid #ea6;
+    margin-top: 6px;
+  }
+
 </style>
